b5d0149dac7b550035ca95fc4357476c602783d9
amirhosein2c <amirhosein.toosi@gmail.com>
2025-11-02 13:52:46 +0330

    sample rate added, webhooks revert to deploy

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker for TB Sense PWA
-const CACHE_NAME = "TB-Sense-Data-Acquisition-v2.4.5";
+const CACHE_NAME = "TB-Sense-Data-Acquisition-v2.5.0";
 const urlsToCache = [
   "/TB-SENSE-DATA-ACQ/",
   "/TB-SENSE-DATA-ACQ/index.html",
