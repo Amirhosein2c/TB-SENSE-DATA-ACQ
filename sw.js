abdb4419b9c5d2039244986fa12cc8c303612cb1
amirhosein2c <amirhosein.toosi@gmail.com>
2025-11-02 11:39:31 +0330

    versioning added

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker for TB Sense PWA
-const CACHE_NAME = "TB-Sense-Data-Acquisition-v2.3.1";
+const CACHE_NAME = "TB-Sense-Data-Acquisition-v2.3.3";
 const urlsToCache = [
   "/TB-SENSE-DATA-ACQ/",
   "/TB-SENSE-DATA-ACQ/index.html",
