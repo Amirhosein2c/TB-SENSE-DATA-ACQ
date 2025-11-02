86e6c67111b61677d060758903459e9c1cc613e1
amirhosein2c <amirhosein.toosi@gmail.com>
2025-11-02 14:41:07 +0330

    storage webhook modified to store tb predicted result, and audio quality

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker for TB Sense PWA
-const CACHE_NAME = "TB-Sense-Data-Acquisition-v2.5.1";
+const CACHE_NAME = "TB-Sense-Data-Acquisition-v2.5.2";
 const urlsToCache = [
   "/TB-SENSE-DATA-ACQ/",
   "/TB-SENSE-DATA-ACQ/index.html",
