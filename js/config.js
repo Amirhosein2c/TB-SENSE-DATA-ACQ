bc598819195d5ebcfce76feaf0df0fc15e730ed7
amirhosein2c <amirhosein.toosi@gmail.com>
2025-11-02 13:19:52 +0330

    webhooks changed to deploy

--- js/config.js
+++ js/config.js
@@ -4,9 +4,8 @@
 const WEBHOOKS = {
   // COUGH_ANALYSIS: "https://api.perceptionist.top/webhook/cough-data-analysis",
   // COUGH_STORAGE: "https://api.perceptionist.top/webhook/cough-data-store",
-  COUGH_ANALYSIS:
-    "https://api.perceptionist.top/webhook-test/cough-data-analysis",
-  COUGH_STORAGE: "https://api.perceptionist.top/webhook-test/cough-data-store",
+  COUGH_ANALYSIS: "https://api.perceptionist.top/webhook/cough-data-analysis",
+  COUGH_STORAGE: "https://api.perceptionist.top/webhook/cough-data-store",
   // Add more webhook URLs here as needed
   // RESULT_SUBMISSION: 'https://api.perceptionist.top/webhook-test/tb_sense_result',
 };
