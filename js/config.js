01c05f0eb8acdd9ac65d2b08137a482820b487ab
amirhosein2c <amirhosein.toosi@gmail.com>
2025-11-02 13:47:30 +0330

    webhooks revert to test

--- js/config.js
+++ js/config.js
@@ -2,10 +2,11 @@
 // Webhook API Endpoints
 
 const WEBHOOKS = {
-  COUGH_ANALYSIS: "https://api.perceptionist.top/webhook/cough-data-analysis",
-  COUGH_STORAGE: "https://api.perceptionist.top/webhook/cough-data-store",
   // COUGH_ANALYSIS: "https://api.perceptionist.top/webhook/cough-data-analysis",
   // COUGH_STORAGE: "https://api.perceptionist.top/webhook/cough-data-store",
+  COUGH_ANALYSIS:
+    "https://api.perceptionist.top/webhook-test/cough-data-analysis",
+  COUGH_STORAGE: "https://api.perceptionist.top/webhook-test/cough-data-store",
   // Add more webhook URLs here as needed
   // RESULT_SUBMISSION: 'https://api.perceptionist.top/webhook-test/tb_sense_result',
 };
