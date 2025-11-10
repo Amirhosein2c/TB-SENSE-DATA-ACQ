dcbacbcc1357fa379182af187539ee74ccfc2c59
amirhosein2c <amirhosein.toosi@gmail.com>
2025-11-10 12:29:27 +0330

    webhook test

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker for TB Sense PWA
-const CACHE_NAME = "TB-Sense-Data-Acquisition-v2.8.3";
+const CACHE_NAME = "TB-Sense-Data-Acquisition-v2.8.4";
 const urlsToCache = [
   "/TB-SENSE-DATA-ACQ/",
   "/TB-SENSE-DATA-ACQ/index.html",
