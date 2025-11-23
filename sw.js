e6decb36d8d8c7fc3dde7d606951cee5c6fe70a8
amirhosein2c <amirhosein.toosi@gmail.com>
2025-11-23 11:37:09 +0330

    webhooks back to deploy - version updated

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker for TB Sense PWA
-const CACHE_NAME = "TB-Sense-Data-Acquisition-v2.9.4";
+const CACHE_NAME = "TB-Sense-Data-Acquisition-v2.9.5";
 const urlsToCache = [
   "/TB-SENSE-DATA-ACQ/",
   "/TB-SENSE-DATA-ACQ/index.html",
