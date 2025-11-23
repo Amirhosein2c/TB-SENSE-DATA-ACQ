7e11a24826ba1d646ac835e806c2562243d228a5
amirhosein2c <amirhosein.toosi@gmail.com>
2025-11-23 11:02:06 +0330

    font sizes decreased

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker for TB Sense PWA
-const CACHE_NAME = "TB-Sense-Data-Acquisition-v2.9.2";
+const CACHE_NAME = "TB-Sense-Data-Acquisition-v2.9.3";
 const urlsToCache = [
   "/TB-SENSE-DATA-ACQ/",
   "/TB-SENSE-DATA-ACQ/index.html",
