3d6577b83696736404b872db1ef55b22b935755e
amirhosein2c <amirhosein.toosi@gmail.com>
2025-11-26 13:20:31 +0330

    Ages sent to left

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker for TB Sense PWA
-const CACHE_NAME = "TB-Sense-Data-Acquisition-v2.10.6";
+const CACHE_NAME = "TB-Sense-Data-Acquisition-v2.10.7";
 const urlsToCache = [
   "/TB-SENSE-DATA-ACQ/",
   "/TB-SENSE-DATA-ACQ/index.html",
