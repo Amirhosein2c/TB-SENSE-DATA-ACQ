ee02eced0536b54b21f102fd77cee04ae85aecf0
Amirhosein Toosi <amirhosein2c@Amirhoseins-MacBook-Pro.local>
2025-12-15 15:29:06 +0330

    merge to main branch

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker for TB Sense PWA
-const CACHE_NAME = "TB-Sense-Data-Acquisition-v2.10.8";
+const CACHE_NAME = "TB-Sense-Data-Acquisition-v2.10.8-dev";
 const urlsToCache = [
   "/TB-SENSE-DATA-ACQ/",
   "/TB-SENSE-DATA-ACQ/index.html",
